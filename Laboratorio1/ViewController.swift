23468b8aeeba40bd3d224312d9ed9465392dfefc
Kenny <kennyc680@gmail.com>
2020-07-05 23:44:52 -0600

    Initial commit

---
+++ Laboratorio1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Laboratorio1
+//
+//  Created by Estudiantes on 7/5/20.
+//  Copyright © 2020 Estudiantes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
